a5454c4ebc7fb9d88d248c076e3da016d2a43acd
Boris Quiroz <boris@insert-coin.org>
2016-03-30 17:30:42 -0300

    add data bag and role path

--- .chef/solo.rb
+++ .chef/solo.rb
@@ -2,4 +2,6 @@ log_level :info
 log_location STDOUT
 file_cache_path "/var/chef/cache"
 cookbook_path ["~/code/chef/cookbooks"]
+data_bag_path "~/code/chef/data_bags"
+role_path "~/code/chef/roles"
 Mixlib::Log::Formatter.show_time = true
