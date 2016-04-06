9bcf65781da8319ce8cdbb64e3b80b099be4f918
Boris Quiroz <boris@insert-coin.org>
2016-04-05 21:16:52 -0300

    my custom config

--- .chef/solo.rb
+++ .chef/solo.rb
@@ -1,7 +1,7 @@
 log_level :info
 log_location STDOUT
 file_cache_path "/var/chef/cache"
-cookbook_path ["~/code/chef/cookbooks"]
-data_bag_path "~/code/chef/data_bags"
-role_path "~/code/chef/roles"
+cookbook_path ["~/Code/chef/cookbooks"]
+data_bag_path "~/Code/chef/data_bags"
+role_path "~/Code/chef/roles"
 Mixlib::Log::Formatter.show_time = true
