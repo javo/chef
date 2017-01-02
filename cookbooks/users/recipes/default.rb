1f206f05bf884c08b1f70a8fbe9aeb7354b4921c
Boris Quiroz <boris@insert-coin.org>
2017-01-02 01:06:35 -0300

    generate random passwd for new users

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -6,6 +6,13 @@ to_add.each do |a|
     action :run
     not_if { File.exists?("/home/#{a}/.bashrc") }
   end
+
+  execute "generate passwd" do
+    passwd = `curl "https://www.random.org/strings/?num=1&len=10&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new"`
+    command "echo '#{a}:#{passwd}' | chpasswd"
+    command "echo ==> passwd for #{a} is #{passwd}"
+    not_if "getent passwd #{a}"
+  end
 end
 
 
