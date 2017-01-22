a9a24e3f1b0140b473865eecfe8555928ddfaaf7
Boris Quiroz <boris@insert-coin.org>
2017-01-22 20:25:50 -0300

    add iptables

--- cookbooks/devops/recipes/default.rb
+++ cookbooks/devops/recipes/default.rb
@@ -1,3 +1,13 @@
-execute "Add iptables rule" do
-  command "iptables -A INPUT -p tcp -s 0/0 --dport #{node["listen"]} -j #{node["policy"]}"
+execute "flush rules" do
+  command "iptables -F"
+end
+
+node['listen'].each do |port|
+  execute "add rules" do
+    command "iptables -A INPUT -p tcp -s 0/0 --dport #{port} -j #{node["policy"]}"
+  end
+end
+
+execute "deny all the rest" do
+  command "iptables -A INPUT -p tcp -s 0/0 -j DROP"
 end
